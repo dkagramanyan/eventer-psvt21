98e86be26632a13b292ae875fe80457720ef9ca3
Карина Гордеева <kagordeeva_1@miem.hse.ru>
2021-09-23 03:20:58 +0300

    feat: add a connection to a google spreadsheet and read its columns to a list

--- parsers/schedule_parser.py
+++ parsers/schedule_parser.py
@@ -1,6 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import logging
+from parsers.configParser import ggl_token_file_name, credentials_file_name, spreadsheet_id, number_orginizers, ranges
 import os
+from datetime import datetime, timezone, timedelta
+
+# Connect logging
+logging.basicConfig(
+    filename='parser.log',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.WARNING
+)
+logger = logging.getLogger(__name__)
 
 
+def get_creds() -> Credentials:
+    """The function to create credentials in order to connect to Google Drive files.
 
-    
-# parser code here
+    :return: credentials
+    :rtype: Credentials
+    """
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive']
+
+    # try to read the credentials from the token file
+    creds = None
+    if os.path.exists(ggl_token_file_name):
+        creds = Credentials.from_authorized_user_file(ggl_token_file_name, SCOPES)
+
+    # create or update credentials and token file
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_file_name, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+
+def get_row_data(spreadsheet_id: str, ranges: str) -> list:
+    """The function to get unformatted, full information rows from a spreadsheet by table id and read ranges.
+
+    :param spreadsheet_id: spreadsheet id
+    :type spreadsheet_id: str
+
+    :param ranges: range of columns as 'A:Z'
+    :type ranges: str
+
+    :return: data rows with unformatted full information from a spreadsheet
+    :rtype: list[...]
+    """
+    credentials = get_creds()
+
+    service = build('sheets', 'v4', credentials=credentials)
+
+    request = service.spreadsheets().get(
+        spreadsheetId=spreadsheet_id,
+        ranges=ranges,
+        includeGridData=True
+    )
+    response = request.execute()
+
+    return response['sheets'][0]['data'][0]['rowData']
+
+
+def get_table(spreadsheet: str, ranges: str) -> list:
+    """The function to get data from a spreadsheet in a readable, unformatted form.
+
+    :param spreadsheet: spreadsheet id
+    :type spreadsheet: str
+
+    :return: list of columns with formatted values or None
+    :rtype: list[[str | None, ...], ...]
+    """
+    table = []
+
+    row_data = get_row_data(
+        spreadsheet_id=spreadsheet,
+        ranges=ranges
+    )
+
+    for row in row_data[:number_orginizers + 1]:
+        try:
+            # filling the table
+            for i, value in enumerate(row['values']):
+                if len(table) <= i:
+                    table.append([])
+
+                if value and 'formattedValue' in value.keys():
+                    formatted_value = value['formattedValue']
+                else:
+                    formatted_value = None
+
+                table[i].append(formatted_value)
+
+        except Exception as e:
+            with open('parser.log', 'a') as f:
+                print(f'{datetime.now(timezone(timedelta(hours=3.0)))} - parsers:schedule_parser - {e}', file=f)
+
+    return table
