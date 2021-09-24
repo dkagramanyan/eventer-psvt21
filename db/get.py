7a02ad7668e110f072ae1f9f59d8a5b03fe50259
Карина Гордеева <kagordeeva_1@miem.hse.ru>
2021-09-24 21:04:13 +0300

    fix: delete useless code

--- db/get.py
+++ db/get.py
@@ -36,15 +36,13 @@ def events_from_db(first_name='', last_name='', all=False) -> list:
     if last_name != '':
         if first_name == 'Фамилии':
             persondb = ssn.query(PersonDB).filter_by(last_name=last_name).first()
-            events = [Event(name=persondb.first_name, surname=persondb.last_name, user_name=persondb.tg_username,
-                            event_name=eventdb.event_name, chat_id=persondb.tg_chat_id, start=eventdb.start,
-                            end=eventdb.end) for eventdb in ssn.query(EventDB).filter_by(person_id=persondb.id)]
 
         else:
             persondb = ssn.query(PersonDB).filter_by(last_name=last_name, first_name=first_name).first()
-            events = [Event(name=persondb.first_name, surname=persondb.last_name, user_name=persondb.tg_username,
-                            event_name=eventdb.event_name, chat_id=persondb.tg_chat_id, start=eventdb.start,
-                            end=eventdb.end) for eventdb in ssn.query(EventDB).filter_by(person_id=persondb.id)]
+
+        events = [Event(name=persondb.first_name, surname=persondb.last_name, user_name=persondb.tg_username,
+                        event_name=eventdb.event_name, chat_id=persondb.tg_chat_id, start=eventdb.start,
+                        end=eventdb.end) for eventdb in ssn.query(EventDB).filter_by(person_id=persondb.id)]
 
     elif all:
 
@@ -65,77 +63,6 @@ def events_from_db(first_name='', last_name='', all=False) -> list:
             events.append(new_event)
 
     return events
-
-
-def schedule_by_name_and_surname(connection: Session, first_name: str, last_name: str) -> str:
-    """The function of getting the text of the message to send to the user by his name and surname.
-
-    :param connection: connection object
-    :type connection: psy._psycopg.connection
-
-    :param first_name: the user's name
-    :type first_name: str
-
-    :param last_name: the user's surname
-    :type last_name: str
-
-    :return: message text with information about the user's events
-    :rtype: str
-    """
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT id "
-                   "FROM plat_people_main "
-                   "WHERE similarity(first_name, %s) > 0.5 AND similarity(middle_name, %s) > 0.5",
-                   (first_name, last_name))
-    person_id = cursor.fetchone()[0]
-
-    cursor.execute("SELECT name, time_from, time_to "
-                   "FROM plat_people_main as ppm "
-                   "JOIN plat_timetable_main as ptm OgitN ppm.id = ptm.org_id "
-                   "JOIN plat_timetable_cases as ptc ON ptm.event_id = ptc.id "
-                   "WHERE org_id = %s", (person_id,))
-    schedule = cursor.fetchall()
-
-    message_text = f'Название: {schedule[0][0]}\n' \
-        f'Время начала: {str(schedule[0][1])[:2]}:{str(schedule[0][1])[2:]}\n' \
-        f'Время конца: {str(schedule[0][2])[:2]}:{str(schedule[0][2])[2:]}'
-
-    return message_text
-
-
-def schedule_by_surname(connection: Session, last_name: str) -> str:
-    """The function of getting the text of the message to send to the user only by his surname.
-
-    :param connection: connection object
-    :type connection: psy._psycopg.connection
-
-    :param last_name: the user's surname
-    :type last_name: str
-
-    :return: message text with information about the user's events
-    :rtype: str
-    """
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT id "
-                   "FROM plat_people_main "
-                   "WHERE similarity(middle_name, %s) > 0.5",
-                   (last_name,))
-    person_id = cursor.fetchone()[0]
-
-    cursor.execute("SELECT name, time_from, time_to "
-                   "FROM plat_people_main as ppm "
-                   "JOIN plat_timetable_main as ptm ON ppm.id = ptm.org_id "
-                   "JOIN plat_timetable_cases as ptc ON ptm.event_id = ptc.id "
-                   "WHERE org_id = %s", (person_id,))
-    schedule = cursor.fetchall()
-
-    message_text = f'Название: {schedule[0][0]}\n' \
-        f'Время начала: {str(schedule[0][1])[:2]}:{str(schedule[0][1])[2:]}\n' \
-        f'Время конца: {str(schedule[0][2])[:2]}:{str(schedule[0][2])[2:]}'
-
-    return message_text
 
 
 def events_to_db(events_from_google: list) -> None:
